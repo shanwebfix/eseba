2d8359d8e513487757f42ba864fdd89ac4ece655
shanwebfix <shanwebfix@gmail.com>
2026-02-19 17:12:18 +0600

    app upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import Hero from "../components/Hero";
 import AppList from "../components/App";
 import MenuList from "../components/Menu";
-import TodayRamadan from "../components/TodayeRamadan";
+
 
 const Home = () => {
   return (
     <div className="space-y-12 animate-fade-in">
       {/* ১. হিরো সেকশন */}
-      <TodayRamadan />
+   
       <Hero />
 
       {/* ২. অ্যাপ লিস্ট সেকশন */}
